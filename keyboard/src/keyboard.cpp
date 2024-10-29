02de1d3b6a99051935418b632c4430ce96911a24
Aleix Martinez <aleixmateumartinez@gmail.com>
2024-10-29 20:52:49 +0100

    new 3d_lidar conf, some modifications and +x permission to keyboard_to_joy script

--- keyboard/src/keyboard.cpp
+++ keyboard/src/keyboard.cpp
@@ -17,74 +17,84 @@ public:
   {
     pub_down = this->create_publisher<keyboard_msgs::msg::Key>("keydown", 10);
     pub_up = this->create_publisher<keyboard_msgs::msg::Key>("keyup", 10);
+
+    // Timer to check for keys pressed
     timer = this->create_wall_timer(20ms, std::bind(&KeyboardNode::timer_callback, this));
 
-    this->declare_parameter("allow_repeat", false);
-    bool allow_repeat = this->get_parameter("allow_repeat").as_bool();
-
-    this->declare_parameter("repeat_delay", SDL_DEFAULT_REPEAT_DELAY);
-    int repeat_delay = this->get_parameter("repeat_delay").as_int();
-
-    this->declare_parameter("repeat_interval", SDL_DEFAULT_REPEAT_INTERVAL);
-    int repeat_interval = this->get_parameter("repeat_interval").as_int();
-   
-
-    if ( !allow_repeat ) repeat_delay=0; // disable
+    if ( !allow_repeat_ ) repeat_delay_=0; // disable
     if (SDL_Init(SDL_INIT_VIDEO) < 0) throw std::runtime_error("Could not init SDL");
-    SDL_EnableKeyRepeat( repeat_delay, repeat_interval );
+    SDL_EnableKeyRepeat( repeat_delay_, repeat_interval_ );
     SDL_WM_SetCaption("ROS keyboard input", NULL);
     window = SDL_SetVideoMode(100, 100, 0, 0);    
 
   }
 
 private:
+  void initParameters() 
+  {
+    this->declare_parameter("allow_repeat", false);
+    allow_repeat_ = this->get_parameter("allow_repeat").as_bool();
+
+    this->declare_parameter("repeat_delay", SDL_DEFAULT_REPEAT_DELAY);
+    repeat_delay_ = this->get_parameter("repeat_delay").as_int();
+
+    this->declare_parameter("repeat_interval", SDL_DEFAULT_REPEAT_INTERVAL);
+    repeat_interval_ = this->get_parameter("repeat_interval").as_int();
+  }
+
   void timer_callback()
   {
 
-    keyboard_msgs::msg::Key k;
+    keyboard_msgs::msg::Key key;
     bool pressed;
 
     bool new_event = false;
   
+    // Get event from SDL lib
     SDL_Event event;
     if (SDL_PollEvent(&event)) {
       switch(event.type) {
       case SDL_KEYUP:
         pressed = false;
-        k.code = event.key.keysym.sym;
-        k.modifiers = event.key.keysym.mod;
+        key.code = event.key.keysym.sym;
+        key.modifiers = event.key.keysym.mod;
         new_event = true;
-        SDL_FillRect(window, NULL,SDL_MapRGB(window->format, k.code, 0, 0));  
+        SDL_FillRect(window, NULL,SDL_MapRGB(window->format, key.code, 0, 0));  
         SDL_Flip(window);   
-	break;
+	      break;
       case SDL_KEYDOWN:
         pressed = true;
-        k.code = event.key.keysym.sym;
-        k.modifiers = event.key.keysym.mod;
+        key.code = event.key.keysym.sym;
+        key.modifiers = event.key.keysym.mod;
         new_event = true;
-        SDL_FillRect(window, NULL, SDL_MapRGB(window->format,k.code,0,255)); 
+        SDL_FillRect(window, NULL, SDL_MapRGB(window->format,key.code,0,255)); 
         SDL_Flip(window);
-
-	break;
+	      break;
       case SDL_QUIT:
-	SDL_FreeSurface(window);
-	SDL_Quit();
-	timer->cancel();
-	break;
+        SDL_FreeSurface(window);
+        SDL_Quit();
+        timer->cancel();
+        break;
       }
     }    
 
     if (new_event) {
-      k.header.stamp = this->get_clock()->now();
-      if (pressed) pub_down->publish(k);
-      else pub_up->publish(k);
+      key.header.stamp = this->get_clock()->now();
+      if (pressed) pub_down->publish(key);
+      else pub_up->publish(key);
     }	
-      
   }
+
+// Params and objects
+private:
   rclcpp::TimerBase::SharedPtr timer;
   rclcpp::Publisher<keyboard_msgs::msg::Key>::SharedPtr pub_down;
   rclcpp::Publisher<keyboard_msgs::msg::Key>::SharedPtr pub_up;
   SDL_Surface* window;
+
+  bool allow_repeat_ = false;
+  int repeat_delay_ = SDL_DEFAULT_REPEAT_DELAY;
+  int repeat_interval_ = SDL_DEFAULT_REPEAT_INTERVAL;
 };
 
 int main(int argc, char * argv[])
